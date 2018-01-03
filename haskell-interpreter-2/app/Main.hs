001c95f217971e0099957fb194c76c0f79c22471
Mac Radigan <mac@radigan.org>
2018-01-02 19:47:50 -0800

    update

--- haskell-interpreter-2/app/Main.hs
+++ haskell-interpreter-2/app/Main.hs
@@ -23,10 +23,23 @@
 
   main :: IO ()
   main = do
+    --
+    -- this works
+    --
     r1 <- runInterpreter testHint
     case r1 of
       Left err -> putStrLn $ errorString err
       Right () -> return ()
+    --
+    -- compilation error:
+    --
+    --     • Couldn't match type ‘[Char]’ with ‘InterpreterT IO ()’
+    --        Expected type: InterpreterT IO ()
+    --        Actual type: String
+    --
+    --     • In the first argument of ‘runInterpreter’, namely ‘content’
+    --       In a stmt of a 'do' block: r2 <- runInterpreter content
+    --       ...
     --
     content <- do readFile "demo/example.hs"
     putStrLn content
